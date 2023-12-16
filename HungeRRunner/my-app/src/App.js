5a84e3c72bbe9fb4f3111fb63d9f75fbf113609e
Hammadmughal12072004 <149532305+Hammadmughal12072004@users.noreply.github.com>
2023-12-17 04:42:18 +0500

    update in the App.js,index.css,index.js files

--- HungeRRunner/my-app/src/App.js
+++ HungeRRunner/my-app/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Homescreen from './Screens/Homescreen';
+import Cartscreen from './Screens/Cartscreen';
+import Registerscreen from './Screens/Registerscreen';
+import Loginscreen from './Screens/Loginscreen';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import Ordersscreen from './Screens/Ordersscreen';
 
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homescreen />} />
+          <Route path="/cart" element={<Cartscreen />} />
+          <Route path="/login" element={<Loginscreen />} />
+          <Route path="/register" element={<Registerscreen />} />
+          <Route path="/orders" element={<Ordersscreen />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
