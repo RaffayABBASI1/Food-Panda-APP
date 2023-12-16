eb33f816836a93b1adc01afde15c08c8d5afae38
RaffayABBASI1 <01-131212-020@student.bahria.edu.pk>
2023-12-16 02:07:11 -0800

    server.js deleted

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const db = require("./db.js")
-const app=express();
-app.use(express.json());
-app.get("/",(req,res)=>{
-    res.send("server-working");
-});
-const port= process.env.PORT || 8000;
-app.listen(port,()=>'server ruuning on port');
